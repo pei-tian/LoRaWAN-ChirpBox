bed7c64d567fbc04c656db802349bd7667159897
Pei Tian <tianpei021@gmail.com>
2021-03-27 22:19:13 +0800

    add stm32ide

---
+++ STM32CubeExpansion_LRWAN_V1.3.1/Projects/STM32L476RG-Nucleo/Applications/LoRa/End_Node/STM32CubeIDE/sx1276mb1mas/Debug/sources.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+EXECUTABLES := 
+OBJS := 
+S_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+OBJCOPY_BIN := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Drivers/BSP/Components \
+Drivers/BSP/IKS01A2 \
+Drivers/BSP/STM32L4xx_Nucleo \
+Drivers/BSP/sx1276mb1mas \
+Drivers/CMSIS \
+Drivers/STM32L4xx_HAL_Driver \
+Middlewares/LoRaWAN/Crypto \
+Middlewares/LoRaWAN/LmHandler \
+Middlewares/LoRaWAN/Mac \
+Middlewares/LoRaWAN/Mac/Region \
+Middlewares/LoRaWAN/Utilities \
+Application/App \
+Application/Core \
+Application/SW4STM32 \
+Application/Target \
+
